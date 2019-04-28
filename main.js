ef0e77d57bf8487b8d3b18bd279e20c29796a1fe
hocine yeddou <hocineyeddou.hy@gmail.com>
2019-04-28 17:55:17 +0200

    add-body

--- main.js
+++ main.js
@@ -1,27 +1,33 @@
-"use strict"
+"use strict";
 
-new Vue({
 
-  el:"#toDolist"
-  data: {
-  todos :[],
-  newlist :''
-  },
+new Vue ({
 
-  methods:{
-
-    addTodo:function(){
-      this.todos.push(this.newlist);
-      this.newList="";
+    el:"#toDolist",
+    data :{
+    
+    todos :[],
+    
+    newlist :''
+    
     },
-
-  filters:{
-
-    capitalize: function(value){
-
-      if(!value) return  ''
-      value =value.toString()
-      return value.charAt(0).toUpperCase() + value.slice(1)
+    methods :{
+    addTodo: function(){
+    this.todos.push(this.newList);
+    this.newList="";
+    },
+    delTodo: function(index){
+        this.todos.splice(index, 1);}
+    },
+    
+    filters: {
+        capitalize: function(value) {
+          if (!value) return ''
+          value = value.toString()
+          return value.charAt(0).toUpperCase() + value.slice(1)
+        }
+    
     }
-  }
-  })
+
+    })
+   
